f2015d3c96bcc12042eed0b530c25d1a7dfc7cc9
Sergey Zasenko <d3fin3@gmail.com>
2016-01-15 00:41:32 +0200

    intelligent format finder

--- format-detector.js
+++ format-detector.js
@@ -51,13 +51,115 @@ FormatFinderOriginal.prototype.find = function (art_dimensions) {
         }
     }
 
-    error = (Math.round(error * 1000.0))/10.0;
-    error += "%";
-    return [rectangle[1], error];
+    return [rectangle[1], this.error2string(error)];
+}
+
+FormatFinderOriginal.prototype.error2string = function(error_num) {
+    var error = (Math.round(error_num * 1000.0))/10.0;
+    return error + "%";
+}
+
+var FormatFinderIntelligent = function () {
+    this.bases = [
+        [1, '1'],
+        [Math.sqrt(2), '√2'],
+        [Math.sqrt(3), '√3'],
+        [Math.sqrt(4), '√4'],
+        [Math.sqrt(5), '√5'],
+    ];
+
+    this.scales = [
+        1, 2, 4
+    ];
+}
+
+FormatFinderIntelligent.prototype = Object.create(FormatFinderOriginal.prototype);
+FormatFinderIntelligent.prototype.constructor = FormatFinderIntelligent;
+
+FormatFinderIntelligent.prototype.find = function(art_dimensions) {
+    var d = art_dimensions.sort(function(a,b) {return b - a});
+    var dp = d[0]/d[1];
+
+    var base = this.bases[0];
+    var scale_error = this.find_base_error(base, dp);
+
+    for (var i = 1; i < this.bases.length; i++) {
+        var test_base = this.bases[i];
+        var test_se   = (this.find_base_error(test_base, dp));
+
+        var diff = Math.abs(test_se[0]) - Math.abs(scale_error[0]);
+        if ( (diff < 0)
+                || (diff == 0 
+                    && Math.abs(test_se[1][0]) < Math.abs(scale_error[1][0]))) {
+            scale_error = test_se;
+            base        = test_base;
+        }
+    }
+
+    // At the end
+    var name = base[1];
+    name += this.scale2string(scale_error[1][0], scale_error[1][1]);
+
+    return [name, this.error2string(scale_error[0])]
+}
+
+FormatFinderIntelligent.prototype.scale2string = function(multiplier, scale) {
+    var name = "";
+    if (multiplier != 0) {
+        if (multiplier > 0) { 
+            name += "+";
+        }
+
+        if (scale == 1) {
+            name += multiplier;
+            return name
+        }
+
+        if (Math.abs(multiplier) > scale) {
+            var whole = 
+                multiplier/Math.abs(multiplier)
+                 *Math.floor(Math.abs(multiplier/scale));
+            name += whole;
+            return name + this.scale2string(multiplier - whole*scale, scale);
+        }
+
+        name += multiplier + "/" + scale;
+    }
+    return name;
+}
+
+FormatFinderIntelligent.prototype.find_base_error = function (base, dp) {
+    var scale = 0;
+    var scale_error = this.find_base_scale_error(base, scale, dp);
+
+    for (var i = 0; i < this.scales.length; i++) {
+        var test_scale = this.scales[i];
+        var test_se    = this.find_base_scale_error(base, test_scale, dp);
+        if (Math.abs(test_se[0]) < Math.abs(scale_error[0])) {
+            scale_error = test_se;
+            scale       = test_scale;
+        }
+    }
+
+    return scale_error;
+}
+
+FormatFinderIntelligent.prototype.find_base_scale_error = function (base, scale, dp) {
+    var total = base[0];
+    var scale_multiplier = 0;
+    if (scale) {
+        scale_multiplier = Math.round((dp-total)*scale);
+        total += scale_multiplier/scale;
+    }
+
+    if (total < 1)
+        total = 1/total;
+
+    return [1-total/dp,[scale_multiplier, scale]]
 }
 
 function show_dimension(name,w,h) {
-    var r = new FormatFinderOriginal().find([w,h]);
+    var r = new FormatFinderIntelligent().find([w,h]);
 
     document.getElementById('art-name').textContent = name;
     document.getElementById('art-dimension').textContent = r[0];
